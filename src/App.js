a50244572a78917d2570aebdcc9f9aaed51a9f91
aman patwal <amanpatwal@amans-MacBook-Air.local>
2024-12-26 14:37:22 +0530

    adding cart details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useDispatch, useSelector } from "react-redux";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { calculateTotal ,getCartItems} from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const {cartItems,isLoading}=useSelector(store=>store.cart);
+  const {isOpen}=useSelector(store=>store.modal);
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(calculateTotal())
+  },[cartItems]);
+
+  useEffect(()=>{
+    dispatch(getCartItems())
+  },[]);
+
+  if(isLoading){
+    return <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  }
+
+
+  return <main>
+    {isOpen && <Modal/>}
+    <Navbar/>
+    <CartContainer/>
+  </main>;
 }
 export default App;
